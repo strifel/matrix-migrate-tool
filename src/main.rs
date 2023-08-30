fa253f79c5fe2464c89fd5f03670f9a338202e05
Felix <info@strifel.de>
2023-08-30 10:26:48 +0200

    move room processing into own function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::{env, process::exit};
+use std::{env, process::exit, collections::HashMap};
 
 use matrix_sdk::{
-    config::SyncSettings, Client, ruma::{events::room::power_levels::RoomPowerLevels, UserId},
+    Client, ruma::{events::room::power_levels::RoomPowerLevels, UserId}, room, config::SyncSettings,
 };
 
 async fn login_and_sync(
@@ -19,9 +19,51 @@ async fn login_and_sync(
 
     println!("logged in as {username}");
 
-    client.sync_once(SyncSettings::default()).await?;
+    let settings = SyncSettings::new().timeout(std::time::Duration::from_secs(300));
+    client.sync_once(settings).await.unwrap();
 
     Ok(client)
+}
+
+async fn process_room(r: &room::Joined, client: &Client, new_user_id: &UserId, leave: bool) -> String {
+    let power_levels: RoomPowerLevels = 
+        r.get_state_event_static()
+          .await.expect("every room has power_level")
+          .unwrap().deserialize().unwrap()
+          .power_levels();
+
+    let power_level = power_levels.for_user(client.user_id().unwrap());
+
+
+
+    if let Err (e) = r.invite_user_by_id(new_user_id).await {
+        println!("Error inviting user: {}", e);
+        if r.is_public() {
+            if r.canonical_alias().is_some() {
+                println!("Room is public: Join via https://matrix.to/#/{}", r.canonical_alias().unwrap().as_str());
+                return String::from("INVITE_ERROR_PUBLIC_ROOM_ALIAS_KNOWN");
+            }
+            return String::from("INVITE_ERROR_PUBLIC_ROOM_ALIAS_UNKNOWN");
+        }
+        return String::from("INVITE_ERROR");
+    }
+
+    if power_level.is_positive() {
+        if let Err (e) = r.update_power_levels(vec![(new_user_id, power_level)]).await {
+            println!("Error updating power levels: {}", e);
+            return String::from("POWER_LEVEL_ERROR");
+        }
+    }
+
+    if leave {
+        if let Err (e) = r.leave().await {
+            println!("Error leaving room: {}", e);
+            return String::from("LEAVE_ERROR");
+        }
+        return String::from("LEFT");
+    } else {
+        return String::from("CAN_LEAVE");
+    }
 }
 
 #[tokio::main]
@@ -45,30 +87,37 @@ async fn main() -> anyhow::Result<()> {
     let new_user_id = <&UserId>::try_from(&*new_user).unwrap();
 
     println!("Synced");
+    let mut room_state = HashMap::new();
 
+
+    let joined_rooms = &client.joined_rooms();
     let mut i = 0;
-    for r in &client.joined_rooms() {
+    for r in joined_rooms {
         let name = r.display_name().await.unwrap();
         println!("{}: {} ({})", i, name, r.room_id());
 
-        let power_levels: RoomPowerLevels = 
-          r.get_state_event_static()
-           .await.expect("every room has power_level")
-           .unwrap().deserialize().unwrap()
-           .power_levels();
-
-        let power_level = power_levels.for_user(client.user_id().unwrap());
-
-        r.invite_user_by_id(new_user_id).await.unwrap();
-        r.update_power_levels(vec![(new_user_id, power_level)]).await.unwrap();
-
-        r.leave().await.unwrap();
-
-        
         i += 1;
     }
 
-    // TODO let user choose chat
+    println!("Selection (comma separated): ");
+
+    let mut line: String = String::new();
+    std::io::stdin().read_line(&mut line)?;
+    line = line.replace("\n", "");
+    line = line.replace(" ", "");
+    let selection_list = line.split(",");
+
+    for rid in selection_list {
+        let selected: usize = rid.replace("!", "").parse().unwrap();
+        let room = &joined_rooms[selected];
+        println!("Processing room {}: {}", rid, room.display_name().await.unwrap());
+        let state = process_room(&room, &client, new_user_id, rid.ends_with("!")).await;
+        room_state.insert(room.room_id(), state);
+    }
+
+    println!("{}", json::stringify_pretty(json::object! {
+        "rooms": room_state
+    }, 4));
 
     Ok(())
 }
